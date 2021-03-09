e50ac1bdd46e6d28319e854db996425cb2759397
yywangchn <yywangchn@outlook.com>
2021-03-09 16:24:18 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -17,12 +17,12 @@ class App extends Component {
       {
         id: 2,
         completed: false,
-        text: "111",
+        text: "222",
       },
       {
         id: 3,
         completed: false,
-        text: "111",
+        text: "333",
       },
     ],
   };
@@ -31,14 +31,38 @@ class App extends Component {
     todos.push({ id: ++nextID, completed: false, value });
     this.setState({ nextID, todos });
   };
+  handleClick = (id) => {
+    let todos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    this.setState({ todos });
+  };
+  handleDelClick = (id) => {
+    const todos = this.state.todos.filter((todo) => todo.id !== id);
+    this.setState({ todos });
+  };
+  handleChange = (value) => {
+    this.setState({ visibilityFilter: value });
+  };
   render() {
-    console.log(this.state);
+    // console.log(this.state);
     return (
       <div className="App">
         <div className="container">
           <AddTodo addTodo={this.addTodo}></AddTodo>
-          {/* <VisibleTodoList></VisibleTodoList> */}
-          {/* <Footer></Footer> */}
+          <VisibleTodoList
+            todos={this.state.todos}
+            visibilityFilter={this.state.visibilityFilter}
+            onClick={this.handleClick}
+            delClick={this.handleDelClick}
+          ></VisibleTodoList>
+          <Footer
+            visibilityFilter={this.state.visibilityFilter}
+            onClick={this.handleChange}
+          ></Footer>
         </div>
       </div>
     );
